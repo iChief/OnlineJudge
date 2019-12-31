a866afcc1e546f38effbacb8c3534916ffe117ac
iChief <ahmedcarlos.schahadeh@students.fhnw.ch>
2019-12-31 11:45:11 +0100

    Solved EuclidProblem ready to sumbit

---
+++ src/main/java/EuclidProblem.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class EuclidProblem {
+    public static void main(String args[]){
+            Scanner in = new Scanner(System.in);
+            while (in.hasNextInt()) {
+                int a = in.nextInt();
+                int b = in.nextInt();
+
+                int x = 0;
+                int y = 1;
+                int x1 = 1;
+                int y1 = 0;
+                while (b != 0) {
+                    int d = a/b;
+
+                    int tmp = b;
+                    b = a%b;
+                    a = tmp;
+
+                    int next_x = x1 - d * x;
+                    x1 = x;
+                    x = next_x;
+
+                    int next_y = y1 - d * y;
+                    y1 = y;
+                    y = next_y;
+                }
+                System.out.println(x1 + " " + y1 + " "  + a);
+            }
+        }
+    }
